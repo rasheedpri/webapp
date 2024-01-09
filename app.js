f4c3bd7c9d1805ebf4e945cc95dc9ad280a35caf
Abdul Rasheed <55779561+rasheedpri@users.noreply.github.com>
2024-01-09 08:08:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!\n');
+  res.end('Hello, World App!\n');
 });
 
 const PORT = 80;
